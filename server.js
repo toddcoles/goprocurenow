d02828065d691d90c191e19c08a84f1412979b40
toddcoles <todd_coles@hotmail.com>
2018-11-29 16:26:31 -0700

    Adding api files

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello'));
 
+// USE ROUTES
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
