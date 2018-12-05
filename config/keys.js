d1b1d0cd7c290faa2dfc12fb7eaa1a91f998ffff
toddcoles <todd_coles@hotmail.com>
2018-12-04 19:47:44 -0700

    Updated server.js file

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://tcoles:nicaud02!@ds243049.mlab.com:43049/vectsupplychain"
+  mongoURI:
+    'mongodb://tcoles:nicaud02!@ds243049.mlab.com:43049/vectsupplychain',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -24,7 +25,12 @@ mongoose
   .then(() => console.log('MongoDB Connected')) // PROMISE
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello')); // ROUTE FOR ALL REQUESTS TO '/'
+// app.get('/', (req, res) => res.send('Hello')); // ROUTE FOR ALL REQUESTS TO '/' -> no longer needed
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // USE ROUTES
 app.use('/api/users', users);
