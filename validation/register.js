40437865e93ef3fcc14ff9440f065389acfb58e2
toddcoles <todd_coles@hotmail.com>
2018-12-09 14:48:22 -0700

    Adding validation

---
+++ validation/register.js
@@ -0,0 +1,15 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegisterInpu(data) {
+  let errors = {};
+
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
