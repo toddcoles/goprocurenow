85bb88715d5879ec35e9ae45b3d80c6b4853107e
toddcoles <todd_coles@hotmail.com>
2018-12-15 18:45:26 -0700

    Adding routing for profiles

--- validation/profile.js
+++ validation/profile.js
@@ -32,30 +32,30 @@ module.exports = function validateProfileInput(data) {
 
   if (!isEmpty(data.youtube)) {
     if (!Validator.isURL(data.youtube)) {
-      errors.youtube = 'Not a valid URL';
+      errors.youtube = 'Not a valid YouTube URL';
     }
   }
 
   if (!isEmpty(data.twitter)) {
     if (!Validator.isURL(data.twitter)) {
-      errors.twitter = 'Not a valid URL';
+      errors.twitter = 'Not a valid Twitter URL';
     }
   }
   if (!isEmpty(data.facebook)) {
     if (!Validator.isURL(data.facebook)) {
-      errors.facebook = 'Not a valid URL';
+      errors.facebook = 'Not a valid Facebook URL';
     }
   }
 
   if (!isEmpty(data.linkedin)) {
     if (!Validator.isURL(data.linkedin)) {
-      errors.linkedin = 'Not a valid URL';
+      errors.linkedin = 'Not a valid Linkedin URL';
     }
   }
 
   if (!isEmpty(data.instagram)) {
     if (!Validator.isURL(data.instagram)) {
-      errors.instagram = 'Not a valid URL';
+      errors.instagram = 'Not a valid Instagram URL';
     }
   }
 
